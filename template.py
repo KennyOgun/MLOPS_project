65ccb48149adf016f3f8fff4fdee9206ce232c8d
KennyOgun <ogundanakehinde2022@gmail.com>
2025-07-25 20:47:05 +0100

    MLOPs folder struture added

---
+++ template.py
@@ -0,0 +1,59 @@
+# template.py
+# This script creates a project structure for a Machine Learning Operations (MLOps) project.
+import os    # Standard library for operating system dependent functionality
+from pathlib import Path   # Object-oriented filesystem paths
+import logging # Standard library for logging messages
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:') # Configure logging to display messages with a timestamp and the message itself
+
+
+project_name = "MlopsProject" # Default project name, can be changed as needed
+
+# Check if the project name is valid
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "test.py"
+
+
+]
+
+# Create directories and files as per the list
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
